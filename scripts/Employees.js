dde20d789efcd8ec3606d18a1f7dedd1ed9e44fb
Ben Shelton <bks-44@hotmail.com>
2022-06-21 12:01:30 -0500

    chapter 5

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,7 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
 
 export const Employees = () => {
     let html = "<ul>"
@@ -14,3 +15,43 @@ export const Employees = () => {
     return html
 }
 
+// End goal: Click employee and name and get ${employee.name} sold # products.
+
+// 
+
+const filterOrdersByEmployee = (employee) => {
+    let arrayOfEmployeeOrders = []
+    for (const order of orders) {
+        if (order.employeeId === employee.id) {
+            arrayOfEmployeeOrders.push (order)
+        }
+    }
+    return arrayOfEmployeeOrders
+}
+
+const employeeOrdersHTML = (arrayOfEmployeeOrders) => {
+    let numberOfOrdersHTML = ""
+    for (let i = 0; i < arrayOfEmployeeOrders.length; i++) {
+        numberOfOrdersHTML ++
+    }
+    return numberOfOrdersHTML
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    const filteredOrders = filterOrdersByEmployee(employee)
+                    const numberOfOrdersByEmployee = employeeOrdersHTML(filteredOrders)
+
+                    window.alert(`${employee.name} sold ${numberOfOrdersByEmployee} products`)
+                }
+            }
+        }
+    }
+)
